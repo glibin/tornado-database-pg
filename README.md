6b1da503951416b37831e220f4536cb9f48cb4e1
Vitaly Glibin <glibin.v@gmail.com>
2012-10-08 17:51:56 +0400

    Initial commit

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-tornado-database-pg
+tornado.database like Postgresql wrapper
 ===================
 
-Postgresql db wrapper for tornado.database
+Postgresql db wrapper similar to tornado.database.
+
+Usefull then migrating from MySQL to Postgresql.
+
+Main difference from tornado.database:
+
+*   `execute` returns `rowcount` not `lastrowid`
+*   you need to specify `RETURNING id;` in `execute_lastrowid` statements
---
+++ database.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python
+#
+# Copyright 2012 Vitaly Glibin
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+"""A lightweight wrapper around psycopg2."""
+
+from __future__ import absolute_import, division, with_statement
+
+import copy
+import itertools
+import logging
+import time
+
+import psycopg2
+
+
+class Connection(object):
+    """A lightweight wrapper around psycopg2 DB-API connections.
+
+    The main value we provide is wrapping rows in a dict/object so that
+    columns can be accessed by name. Typical usage::
+
+        db = database.Connection("localhost", "mydatabase")
+        for article in db.query("SELECT * FROM articles"):
+            print article.title
+
+    Cursors are hidden by the implementation, but other than that, the methods
+    are very similar to the DB-API.
+
+    We explicitly set the timezone to UTC and the character encoding to
+    UTF-8 on all connections to avoid time zone and encoding errors.
+    """
+    def __init__(self, host, database, user=None, password=None):
+        self.host = host
+        self.database = database
+
+        args = dict(database = database)
+
+        if user is not None:
+            args["user"] = user
+        if password is not None:
+            args["password"] = password
+
+
+        pair = host.split(":")
+        if len(pair) == 2:
+            args["host"] = pair[0]
+            args["port"] = int(pair[1])
+        else:
+            args["host"] = host
+            args["port"] = 5432
+
+        self._db = None
+        self._db_args = args
+        try:
+            self.reconnect()
+        except Exception:
+            logging.error("Cannot connect to PostgreSQL on %s", self.host,
+                exc_info=True)
+
+    def __del__(self):
+        self.close()
+
+    def close(self):
+        """Closes this database connection."""
+        if getattr(self, "_db", None) is not None:
+            self._db.close()
+            self._db = None
+
+    def reconnect(self):
+        """Closes the existing database connection and re-opens it."""
+        self.close()
+        self._db = psycopg2.connect(**self._db_args)
+        self._db.autocommit = True
+        self.execute("SET TIME ZONE +0")
+
+    def iter(self, query, *parameters):
+        """Returns an iterator for the given query and parameters."""
+        self._ensure_connected()
+        cursor = self._cursor()#MySQLdb.cursors.SSCursor(self._db)
+        try:
+            self._execute(cursor, query, parameters)
+            column_names = [d[0] for d in cursor.description]
+            for row in cursor:
+                yield Row(zip(column_names, row))
+        finally:
+            cursor.close()
+
+    def query(self, query, *parameters):
+        """Returns a row list for the given query and parameters."""
+        cursor = self._cursor()
+        try:
+            self._execute(cursor, query, parameters)
+            column_names = [d[0] for d in cursor.description]
+            return [Row(itertools.izip(column_names, row)) for row in cursor]
+        finally:
+            cursor.close()
+
+    def get(self, query, *parameters):
+        """Returns the first row returned for the given query."""
+        rows = self.query(query, *parameters)
+        if not rows:
+            return None
+        elif len(rows) > 1:
+            raise Exception("Multiple rows returned for Database.get() query")
+        else:
+            return rows[0]
+
+    def execute(self, query, *parameters):
+        """Executes the given query, returning the lastrowid from the query."""
+        return self.execute_rowcount(query, *parameters)
+
+    def execute_lastrowid(self, query, *parameters):
+        """Executes the given query, returning the lastrowid from the query."""
+        cursor = self._cursor()
+        try:
+            self._execute(cursor, query, parameters)
+            return cursor.fetchone()[0]#cursor.lastrowid
+        finally:
+            cursor.close()
+
+    def execute_rowcount(self, query, *parameters):
+        """Executes the given query, returning the rowcount from the query."""
+        cursor = self._cursor()
+        try:
+            self._execute(cursor, query, parameters)
+            return cursor.rowcount
+        finally:
+            cursor.close()
+
+    def executemany(self, query, parameters):
+        """Executes the given query against all the given param sequences.
+
+        We return the lastrowid from the query.
+        """
+        return self.executemany_rowcount(query, parameters)
+
+    def executemany_lastrowid(self, query, parameters):
+        """Executes the given query against all the given param sequences.
+
+        We return the lastrowid from the query.
+        """
+        cursor = self._cursor()
+        try:
+            cursor.executemany(query, parameters)
+            return cursor.fetchone()[0]#cursor.lastrowid
+        finally:
+            cursor.close()
+
+    def executemany_rowcount(self, query, parameters):
+        """Executes the given query against all the given param sequences.
+
+        We return the rowcount from the query.
+        """
+        cursor = self._cursor()
+        try:
+            cursor.executemany(query, parameters)
+            return cursor.rowcount
+        finally:
+            cursor.close()
+
+    def _ensure_connected(self):
+        if (self._db is None or
+            self._db.closed):
+            self.reconnect()
+
+    def _cursor(self):
+        self._ensure_connected()
+        return self._db.cursor()
+
+    def _execute(self, cursor, query, parameters):
+        try:
+            return cursor.execute(query, parameters)
+        except OperationalError:
+            logging.error("Error connecting to PostgreSQL on %s", self.host)
+            self.close()
+            raise
+
+
+class Row(dict):
+    """A dict that allows for object-like property access syntax."""
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
+
+IntegrityError = psycopg2.IntegrityError
+OperationalError = psycopg2.OperationalError
